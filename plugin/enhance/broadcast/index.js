0ba833dc541a45699409ac5ffd7ca41584872d36
neta <neta520@163.com>
2022-05-07 18:21:45 +0800

    05/07

--- plugin/enhance/broadcast/index.js
+++ plugin/enhance/broadcast/index.js
@@ -2,13 +2,7 @@ const schedule = require('node-schedule')
 const { sendGroupMsg } = require('../../../api/requests')
 const printMessage = require('./printMessage')
 
-const groupList = [
-    '771671566', // 四群
-    '906026830', // 原神
-    '672269736', // 三好学生
-    '903094947', // 家人们
-    '302391473', // 后援会
-]
+const groupList = []
 const time = 12 // 每日12点
 
 /**
